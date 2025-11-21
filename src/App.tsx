3e6a28feb3885b9f1ce0943714cb5b4d5654b58f
Vardhu32 <vardhu9550@gmail.com>
2025-11-21 09:09:05 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ function App() {
     <div className="min-h-screen bg-black text-white overflow-x-hidden">
       <div className="fixed inset-0 bg-gradient-to-b from-black via-gray-900 to-black opacity-50 pointer-events-none" />
 
-      {isStrangerThingsMode && <StrangerThingsBackground />}
       {isStrangerThingsMode && <StrangerThingsSky />}
       {isStrangerThingsMode && <RedSmokeBackground />}
+      {isStrangerThingsMode && <StrangerThingsBackground />}
       
       <div className="relative z-10">
         <Navigation  
